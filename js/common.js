253d3bcdd68f198342f2ef9bf4e73bbe484243e5
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-12-24 11:53:01 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function ($) {
+
+  $(".up").click(function () {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top - 110;
+    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 500);
+    return false;
+  });
+  //Действие при кролле
+  $(window).scroll(function() { 
+    if ($(window).scrollTop() > 520) {
+      $('.up').fadeIn(300);
+    } else {
+      $('.up').fadeOut(300);
+    }
+  });
+
+  $('.constructor-wrap-item').on('click', function() {
+    $('.constructor-wrap-item').removeClass('constructor-wrap-item-active');
+    $('.constructor-buttons .notice').fadeOut(200);
+    $('.constructor-buttons .button').removeClass('deactive');
+    $(this).addClass('constructor-wrap-item-active');
+
+    $('.constructor-buttons .button').attr('href', $(this).attr('data-src'));
+    
+  });
+  
+  $('.play').on('click', function() { 
+    $('.play img').fadeOut(200);
+    $('.play iframe').fadeIn(300);
+    $('.play iframe').attr('src', $('.play iframe').attr('data-src'));
+  });
+
+  /* $('.assets-d').on('click', function() { 
+    $('.overlay').fadeIn(200);
+    $('.popup').fadeIn(200);
+  }); */
+
+  if ($(window).width() > 992) {
+
+    //Действие при кролле
+    /* $(window).scroll(function() { 
+      if ($(window).scrollTop() > 160) {
+      } else {
+      }
+    }); */
+
+    $(".header__menu ul li a").click(function () {
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top - 110;
+      $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 300);
+      return false;
+    });
+
+    $(".offer a").click(function () {
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top - 110;
+      $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 300);
+      return false;
+    });
+
+  } else {
+    jQuery('.burger').on('click', function() {
+      jQuery(this).toggleClass('burger-active');
+      jQuery('.header__menu').slideToggle(150);
+      jQuery('.header__call').fadeToggle(150);
+    });
+
+    jQuery(".header__menu ul li a").click(function () {
+      var elementClick = jQuery(this).attr("href");
+      var destination = jQuery(elementClick).offset().top - 110;
+      jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 300);
+      jQuery('.header__menu').slideUp(200);
+      jQuery('.burger').removeClass('burger-active');
+      jQuery('.header__call').fadeOut(150);
+
+      return false;
+    });
+    jQuery(".offer a").click(function () {
+      var elementClick = jQuery(this).attr("href");
+      var destination = jQuery(elementClick).offset().top - 110;
+      jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 300);
+      jQuery('.header__menu').slideUp(200);
+      jQuery('.burger').removeClass('burger-active');
+      return false;
+    });
+  }
+
+  $('.close').on('click', function() {
+    $('.overlay').fadeOut(200);
+    $('.popup').fadeOut(200);
+  });
+  $('.overlay').on('click', function() {
+    $('.overlay').fadeOut(200);
+    $('.popup').fadeOut(200);
+  });
+
+
+});
